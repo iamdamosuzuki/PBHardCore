d1db296a0a4bcce51bab24bcfac386315d3b08d2
iamdamosuzuki <iownafordprobe@gmail.com>
2014-10-16 21:17:53 -0400

    working validation script

--- Scripts/PBCValidate.sh
+++ Scripts/PBCValidate.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-echo $1
-
-/usr/bin/xmllint --noout --schema http://www.pbcore.org/xsd/pbcore-2.0.xsd $1
+/usr/bin/xmllint --noout --schema http://www.pbcore.org/xsd/pbcore-2.0.xsd "$1"
